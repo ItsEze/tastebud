18162b383a8c00a436c3509639f2a9174682edf3
itsEze <tylermkendall@gmail.com>
2023-10-27 00:51:27 -0500

    aws bug 1

--- tastebud_frontend/src/api/authApi.js
+++ tastebud_frontend/src/api/authApi.js
@@ -15,7 +15,7 @@ async function basicFetch(url, payload) {
       },
       body: JSON.stringify(context)
     }
-    const body = await basicFetch(`${base_url}users/signup/`,payload)
+    const body = await basicFetch(`${base_url}:80/users/signup/`,payload)
     return body
   }
   
@@ -29,7 +29,7 @@ async function basicFetch(url, payload) {
       },
       body: JSON.stringify(context)
     }
-    const body = await basicFetch(`${base_url}users/get-token/`, payload)
+    const body = await basicFetch(`${base_url}/users/get-token/`, payload)
     return body.token
   }
 
@@ -45,7 +45,7 @@ async function basicFetch(url, payload) {
       })
       .join('&');
   
-    const url = `${base_url}api/v1/recipe_search/?${queryString}`;
+    const url = `${base_url}/api/v1/recipe_search/?${queryString}`;
   
     const payload = {
       method: 'GET',
