1df9ee904de20783e5f2407a3338129a5086f0d1
ItsEze <tylermkendall@gmail.com>
2024-04-10 02:41:16 -0500

    fixed base url in settings

--- tastebud_django/tastebud/settings.py
+++ tastebud_django/tastebud/settings.py
@@ -155,7 +155,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     'http://3.138.124.217',
-    'http://18.191.234.165/',
+    'http://18.191.234.165',
+    'http://18.191.165.61'
 ]
 
 CORS_ALLOW_HEADERS = [
